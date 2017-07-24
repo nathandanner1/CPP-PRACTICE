8f7daad4948eb86802b2110d16815074e0d93f7a
nathandanner1 <30420495+nathandanner1@users.noreply.github.com>
2017-07-24 18:59:38 -0400

    Name Selection
    Added name selection input

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	char a[50];
+	cout << "Hello, welcome to generic place game is located." << endl;
+	cout << "Could you enter your name please?: ";
+	cin >> a;
+	cout << "Oh, so your name is " << a << ", I see. " << endl;
+	cout << "Hello there " << a << ", welcome to the game" << endl;
+
+	system("pause");
+
+}
